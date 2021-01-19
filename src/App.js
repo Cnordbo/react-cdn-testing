e0f1d9efb931d07072063271e1d33d4e92063c87
Christer Nordbø <cnordbo@gmail.com>
2021-01-19 23:17:45 +0100

    Git PR 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pull Request 1
+          Pull Request 2
         </p>
         <a
           className="App-link"
